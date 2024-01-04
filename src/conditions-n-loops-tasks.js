ecd6880874f7a300523ee4b7d86c7a90cd96653d
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-01-04 21:31:22 +0300

    feat: 5 done

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -253,8 +253,13 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = String(num);
+  const strD = String(digit);
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === strD) return true;
+  }
+  return false;
 }
 
 /**
